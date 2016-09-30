51ad541c41f0f4923e827d3b6dc421b5a64209ff
Agnė Klimaitė <agnaite@me.com>
2016-09-29 21:20:41 -0700

    removed heroku setup from setupdb

--- setupdb.py
+++ setupdb.py
@@ -1,25 +1,11 @@
 # run database migrations and what-not
 
 from environment import *
-import os
-import psycopg2
-import urlparse
 
 print(db)
 
 db.connect()
 db.create_tables([User, Message])
-
-# urlparse.uses_netloc.append("postgres")
-# url = urlparse.urlparse(os.environ["DATABASE_URL"])
-
-# conn = psycopg2.connect(
-#     database=url.path[1:],
-#     user=url.username,
-#     password=url.password,
-#     host=url.hostname,
-#     port=url.port
-# )
 
 # some seeds go here
 
